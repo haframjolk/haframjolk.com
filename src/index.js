2cbc133ee7d0f5a85661a7def6776fddcf8d38a4
Reyn Alpha <reynalpha@outlook.com>
2020-11-24 17:30:18 +0000

    Add contact form with button

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import KonamiCode from 'konami-code';
 import spawnKickAss from './lib/kickass';
+import Modal from './lib/modal';
 
 document.addEventListener('DOMContentLoaded', () => {
   // Spawn Kick Ass if Konami code is entered
   const konami = new KonamiCode();
   konami.listen(spawnKickAss);
+
+  // Activate contact form modal
+  const contactModal = new Modal(document.querySelector('.dialog--contact'));
+  const contactButton = document.querySelector('#contact-button');
+  contactButton.addEventListener('click', contactModal.open.bind(contactModal));
 });
---
+++ src/lib/modal.js
@@ -0,0 +1,38 @@
+export default class Modal {
+  /**
+   * Creates a new Modal object
+   *
+   * @param {HTMLDialogElement} element Dialog element used by modal
+   */
+  constructor(element) {
+    this.element = element;
+
+    // Events
+    this.element.addEventListener('click', (e) => {
+      if (e.target === this.element) {
+        this.close();
+      }
+    });
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        this.close();
+      }
+    });
+  }
+
+  /**
+   * Opens the modal
+   */
+  open() {
+    this.element.setAttribute('open', '');
+    document.body.classList.add('body--modal-open');
+  }
+
+  /**
+   * Closes the modal
+   */
+  close() {
+    this.element.removeAttribute('open');
+    document.body.classList.remove('body--modal-open');
+  }
+}
