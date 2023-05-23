b2756c0a9accafcf7357676e40bb2e625f3092c3
Reyn Alpha <reynalpha@outlook.com>
2023-05-23 17:46:32 +0000

    Use defer on script

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import Konami from 'konami';
 import spawnKickAss from './lib/kickass';
 import Modal from './lib/modal';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Spawn Kick Ass if Konami code is entered
-  Konami(spawnKickAss);
+// Spawn Kick Ass if Konami code is entered
+Konami(spawnKickAss);
 
-  // Activate contact form modal
-  const contactModal = new Modal(document.querySelector('.dialog--contact'));
-  const contactButton = document.querySelector('#contact-button');
-  contactButton.addEventListener('click', contactModal.open.bind(contactModal));
+// Activate contact form modal
+const contactModal = new Modal(document.querySelector('.dialog--contact'));
+const contactButton = document.querySelector('#contact-button');
+contactButton.addEventListener('click', contactModal.open.bind(contactModal));
 
-  // Close contact form on submission
-  const contactForm = document.querySelector('.dialog--contact__form');
-  contactForm.addEventListener('submit', contactModal.close.bind(contactModal));
-});
+// Close contact form on submission
+const contactForm = document.querySelector('.dialog--contact__form');
+contactForm.addEventListener('submit', contactModal.close.bind(contactModal));
